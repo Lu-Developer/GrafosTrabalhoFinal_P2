aaf85bc7fbffe8b3ae1a31acf37b850cd00aadd3
Luana Nietto <luannietto15@gmail.com>
2023-11-22 19:47:56 -0300

    Test Commit

--- src/telas/Configuracao.java
+++ src/telas/Configuracao.java
@@ -1,4 +1,17 @@
 package telas;
 
 public class Configuracao {
+    private String teste;
+
+    public Configuracao(String teste) {
+        this.teste = teste;
+    }
+
+    public String getTeste() {
+        return teste;
+    }
+
+    public void setTeste(String teste) {
+        this.teste = teste;
+    }
 }
